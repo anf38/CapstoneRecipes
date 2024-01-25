e42e614c3911215f83803abc8e9a3fa488a11efa
amilia floate <anf38@students.uwf.edu>
2024-01-25 14:37:29 -0600

    Setting up the code

--- recipePage/src/main/java/com/example/recipepage/recipePage.java
+++ recipePage/src/main/java/com/example/recipepage/recipePage.java
@@ -6,6 +6,7 @@ public class recipePage {
         System.out.println("hello world ");
 
         layout myLayout = new layout ("javafx or swing?");
+        myLayout.setModel("both!");
         System.out.println(myLayout.getModel());
     }
 }
