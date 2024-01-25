30fcf38bf34ba041b1ba77f8e1348bfca93725c7
amilia floate <anf38@students.uwf.edu>
2024-01-25 14:25:15 -0600

    Setting up the code

---
+++ recipePage/src/main/java/com/example/recipepage/recipePage.java
@@ -0,0 +1,10 @@
+package com.example.recipepage;
+import com.example.recipepage.model.layout;
+public class recipePage {
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+
+        layout myLayout = new layout ("javafx or swing?");
+        System.out.println(myLayout.getModel());
+    }
+}
