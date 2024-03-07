07c8473c108eebad53a76cfe893cdb3ceddd0ffb
amilia floate <anf38@students.uwf.edu>
2024-03-06 20:40:13 -0600

    got the navigation bar to work and submitting a recipe roughly works

--- app/src/main/java/com/example/pantrypal/MainActivity.java
+++ app/src/main/java/com/example/pantrypal/MainActivity.java
@@ -1,51 +1,72 @@
 package com.example.pantrypal;
-
 import android.content.Intent;
+import android.content.om.FabricatedOverlay;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 public class MainActivity extends AppCompatActivity {
-
     FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
+    BottomNavigationView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
+        nav = findViewById(R.id.nav);
+        nav.setSelectedItemId(R.id.homeIcon);
 
-//        if(auth.getCurrentUser() == null) {
-//            Intent intent = new Intent(getApplicationContext(), Login.class);
-//            startActivity(intent);
-//            finish();
-//        }
-//        else {
-//            user = auth.getCurrentUser();
-//            textView.setText(user.getEmail());
-//        }
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+
+
+        //goal: make it so that the screen outputs the top 4 most recently created recipes
+
+
+
+
+
+
+
+
+
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-                finish();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.homeIcon) {
+                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_LONG).show();
+                    // Handle navigation to Home activity if necessary
+                    return true;
+                } else if (itemId == R.id.searchIcon) {
+                    Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_LONG).show();
+                    // Handle navigation to Search activity
+                    startActivity(new Intent(MainActivity.this, Search.class));
+                    return true;
+                } else if (itemId == R.id.favoriteIcon) {
+                    Toast.makeText(MainActivity.this, "Favorite", Toast.LENGTH_LONG).show();
+                    // Handle navigation to Favorite activity
+                    startActivity(new Intent(MainActivity.this, Favorites.class));
+                    return true;
+                } else if (itemId == R.id.submissionIcon) {
+                    Toast.makeText(MainActivity.this, "Submission", Toast.LENGTH_LONG).show();
+                    // Handle navigation to Submission activity
+                    startActivity(new Intent(MainActivity.this, NewRecipe.class));
+                    return true;
+                } else if (itemId == R.id.ingredientsIcon) {
+                    Toast.makeText(MainActivity.this, "Ingredients", Toast.LENGTH_LONG).show();
+                    // Handle navigation to Ingredients activity
+                    startActivity(new Intent(MainActivity.this, IngredientsSearch.class));
+                    return true;
+                }
+                return false;
             }
         });
     }
 }
-
