833d0836e06ee5fbbfafd476ddd83dbefd2d6dd3
amilia floate <anf38@students.uwf.edu>
2024-03-18 09:59:33 -0500

    working Search.java

--- app/src/main/java/com/example/pantrypal/Search.java
+++ app/src/main/java/com/example/pantrypal/Search.java
@@ -1,4 +1,53 @@
 package com.example.pantrypal;
 
-public class Search {
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Search extends AppCompatActivity {
+
+    ListView listView;
+    String[] name = {"amilia", "anthony", "nick", "jadson", "emily", "daryl", "tish", "lauren", "jon", "maria", "cyan", "princeton"};
+
+    ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        listView = findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,name);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
 }
+
+
