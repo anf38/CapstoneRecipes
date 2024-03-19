c7c7b0dfc370b834b07073396a5c1b854da25731
amilia floate <anf38@students.uwf.edu>
2024-03-19 14:20:13 -0500

    working Search.java w/ firebase

--- app/src/main/java/com/example/pantrypal/Search.java
+++ app/src/main/java/com/example/pantrypal/Search.java
@@ -7,24 +7,58 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Search extends AppCompatActivity {
 
+    FirebaseFirestore fStore;
     ListView listView;
-    String[] name = {"amilia", "anthony", "nick", "jadson", "emily", "daryl", "tish", "lauren", "jon", "maria", "cyan", "princeton"};
-
     ArrayAdapter<String> arrayAdapter;
+    List<String> recipeNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        fStore = FirebaseFirestore.getInstance();
 
         listView = findViewById(R.id.listView);
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,name);
+        //arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, name);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, recipeNames);
         listView.setAdapter(arrayAdapter);
+
+        fetchRecipesFromFirestore();
     }
+
+    private void fetchRecipesFromFirestore() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("recipes").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String recipeName = document.getString("Name");
+                                recipeNames.add(recipeName);
+                            }
+                            arrayAdapter.notifyDataSetChanged(); // Notify adapter after data is fetched
+                        } else {
+                            // Handle errors
+                        }
+                    }
+                });
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,5 +83,3 @@ public class Search extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 }
-
-
