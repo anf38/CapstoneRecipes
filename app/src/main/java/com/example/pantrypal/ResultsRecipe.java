78bd4f42c72a4f96743a215d689e360836e4e9c8
Keaphs Nguyen <keaphsn@gmail.com>
2024-04-14 12:46:21 -0500

    removed unused imports

--- app/src/main/java/com/example/pantrypal/ResultsRecipe.java
+++ app/src/main/java/com/example/pantrypal/ResultsRecipe.java
@@ -1,7 +1,5 @@
 package com.example.pantrypal;
 
-import android.net.Uri;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
--- app/src/main/java/com/example/pantrypal/activities/Search.java
+++ app/src/main/java/com/example/pantrypal/activities/Search.java
@@ -1,18 +1,13 @@
 package com.example.pantrypal.activities;
 
-import static android.widget.Toast.makeText;
-
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
