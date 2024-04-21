0748f454dfb2b56e699ab77b1c0dc5881e1409db
Tomasz Bialek <tomektbialek@gmail.com>
2024-04-21 18:18:06 -0500

    removed unused imports

--- app/src/main/java/com/example/pantrypal/activities/ViewRecipe.java
+++ app/src/main/java/com/example/pantrypal/activities/ViewRecipe.java
@@ -2,8 +2,6 @@ package com.example.pantrypal.activities;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,15 +16,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pantrypal.Comment;
 import com.example.pantrypal.CommentsListAdapter;
 import com.example.pantrypal.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
